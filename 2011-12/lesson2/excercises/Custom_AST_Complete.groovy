c57bbb0a2f4b90f091c2c38f6fd6330265bcb008
vaclav.pech@gmail.com <vaclav.pech@gmail.com@be7033a9-59cf-b609-fd22-ae6033124dc4>
2011-10-13 09:20:46 +0000

    Polished the demos

--- 2011-12/lesson2/excercises/Custom_AST_Complete.groovy
+++ 2011-12/lesson2/excercises/Custom_AST_Complete.groovy
@@ -10,6 +10,8 @@ import org.codehaus.groovy.ast.stmt.*
 import static org.codehaus.groovy.control.CompilePhase.*
 import org.codehaus.groovy.ast.builder.*
 import org.codehaus.groovy.ast.expr.*
+import org.codehaus.groovy.syntax.*
+import org.codehaus.groovy.control.messages.*
 
 @Retention (RetentionPolicy.SOURCE)
 @Target ([ElementType.METHOD])
@@ -22,9 +24,30 @@ public @interface Requires {
 @GroovyASTTransformation(phase = SEMANTIC_ANALYSIS)
 public class RequiresTransformation implements ASTTransformation {
 
+    def annotationType = Requires.class.name
+
+    private boolean checkNodes(ASTNode[] astNodes, String annotationType) {
+        if (! astNodes)    return false
+        if (! astNodes[0]) return false
+        if (! astNodes[1]) return false
+        if (!(astNodes[0] instanceof AnnotationNode))        return false
+        if (! astNodes[0].classNode?.name == annotationType) return false
+        if (!(astNodes[1] instanceof MethodNode))            return false
+        true
+    }
+    
     public void visit(ASTNode[] astNodes, SourceUnit source) {
+        if (!checkNodes(astNodes, annotationType)) {
+            addError("Internal error on annotation", astNodes[0], sourceUnit);
+            return
+        }
         MethodNode annotatedMethod = astNodes[1]
         def annotationExpression = astNodes[0].members.value
+
+        if (annotationExpression.class != ConstantExpression) {
+            addError("The condition is not a constant expression", astNodes[0], sourceUnit);
+            return
+        }
 
         String annotationValueString = annotationExpression.value
         BlockStatement block = createStatements(annotationValueString)
@@ -45,20 +68,21 @@ public class RequiresTransformation implements ASTTransformation {
         BlockStatement bs = res[0]
         return bs
     }
+    
+    public void addError(String msg, ASTNode expr, SourceUnit source) {
+        int line = expr.lineNumber
+        int col = expr.columnNumber
+        SyntaxException se = new SyntaxException(msg + '\n', line, col)
+        SyntaxErrorMessage sem = new SyntaxErrorMessage(se, source)
+        source.errorCollector.addErrorAndContinue(sem)
+    }    
 }
 
 final calculator = new GroovyShell(this.class.getClassLoader()).evaluate('''
-
-
 class Calculator {
     @Requires("divisor != 0")
     public int divide10By(divisor) {
         10/divisor
-    }
-
-    @Requires("a > b")
-    public int subtract(a, b) {
-        a - b
     }
 }
 
@@ -66,8 +90,4 @@ new Calculator()
 ''')
 
 println calculator.divide10By(5)
-println calculator.subtract(10, 5)
-
-//TASK uncomment the following lines and see the reported problems, then compare with the error you'd get without the applying the transformation to the divide10By() method
-//println calculator.divide10By(0)
-//println calculator.subtract(5, 10)
+println calculator.divide10By(0)
