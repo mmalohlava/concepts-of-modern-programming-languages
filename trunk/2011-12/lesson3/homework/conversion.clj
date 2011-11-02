f4e2634a9700dfdeb00cb75d0d424b635a96d3be
michal.malohlava@gmail.com <michal.malohlava@gmail.com@be7033a9-59cf-b609-fd22-ae6033124dc4>
2011-11-02 12:04:10 +0000

    minor correction in the text

--- trunk/2011-12/lesson3/homework/conversion.clj
+++ trunk/2011-12/lesson3/homework/conversion.clj
@@ -1,14 +1,17 @@
 (ns nprg014.conversion)
 
-; write a DSL which allows users to define conversion rules
+; Write a DSL which allows users to define conversion rules specified in the following form: 
 (defunits-of distance :m ; rules for distance, the base unit is :m (one meter)
   :km 1000
   :cm 1/100 ; Clojure allows rationals
   :mm 1000
   :ft 0.3048
   :mile 1 609 )
+  
+; The conversion rules are called distance and the base unit is :m representing 1 meter. 
+; A conversion rule for given unit defines a relation of the unit to meters (e.g., 1 km is 1000 m). 
 
-; which prepares function/macro units-of-distance which allow users to convert given value to given unit 
+; The macro should prepare a new function/macro units-of-distance which allow users to convert given value to given unit 
 ; For example:
 (unit-of-distance 1 :km) ; shoudl return 1000
 (unit-of-distance 2 :cm) ; should return 2/100
