c5d1b19afd9fc28cefde946aedda112c514cb7db
vaclav.pech@gmail.com <vaclav.pech@gmail.com@be7033a9-59cf-b609-fd22-ae6033124dc4>
2011-12-14 11:19:46 +0000

    Homework update

--- trunk/2011-12/lesson6/homework/G14_FibonacciStatefulActors.groovy
+++ trunk/2011-12/lesson6/homework/G14_FibonacciStatefulActors.groovy
@@ -1,2 +1,2 @@
-//TODO Implement a Fibonacci number generator using Stateless actors
+//TODO Implement a Fibonacci number generator using either Stateful actors, Stateless actors or Active objects
 
